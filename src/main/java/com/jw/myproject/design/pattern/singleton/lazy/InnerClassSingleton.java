d76a07f18a9cc661392c666e5d8f24fd3613f04b
lijw <lijw@fxqifu.com>
2020-10-23 17:55:32 +0800

    singleton

---
+++ src/main/java/com/jw/myproject/design/pattern/singleton/lazy/InnerClassSingleton.java
@@ -0,0 +1,36 @@
+package com.jw.myproject.design.pattern.singleton.lazy;
+
+/**
+ * @author lijw
+ * @date 2020/10/23 9:55
+ */
+public class InnerClassSingleton {
+
+    static {
+        System.out.println("外部类静态块开始睡2秒..........");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("外部类进行了初始化！");
+    }
+    static InnerClassSingleton getInstance() {
+        return InnerClass.SINGLETON;
+    }
+
+    private static class InnerClass {
+        static {
+            System.out.println("静态内部类静态块开始睡2秒..........");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            SINGLETON = new InnerClassSingleton();
+            System.out.println("静态内部类进行了初始化！");
+        }
+
+        private static final InnerClassSingleton SINGLETON;
+    }
+}
---
+++ src/main/java/com/jw/myproject/design/pattern/singleton/lazy/InnerClassTest.java
@@ -0,0 +1,22 @@
+package com.jw.myproject.design.pattern.singleton.lazy;
+
+/**
+ * @author lijw
+ * @date 2020/10/23 10:08
+ */
+public class InnerClassTest {
+
+    public static void main(String[] args) {
+//        Thread t1 = new Thread(() -> {
+            InnerClassSingleton s = InnerClassSingleton.getInstance();
+            System.out.println(s);
+//        }, "线程1");
+//        Thread t2 = new Thread(() -> {
+//            InnerClassSingleton s = InnerClassSingleton.getInstance();
+//            System.out.println(s);
+//        }, "线程2");
+
+//        t1.start();
+//        t2.start();
+    }
+}
