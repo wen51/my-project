ad8a20d49c75a0dbbf2a4933cb6bbfd9d5a80274
wen <1065281047@qq.com>
2020-11-04 22:48:35 +0800

    开始cglib

---
+++ src/main/java/com/jw/myproject/pattern/proxy/cglibproxy/CGlibMeipo.java
@@ -0,0 +1,13 @@
+package com.jw.myproject.pattern.proxy.cglibproxy;
+
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CGlibMeipo implements MethodInterceptor {
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        return null;
+    }
+}
