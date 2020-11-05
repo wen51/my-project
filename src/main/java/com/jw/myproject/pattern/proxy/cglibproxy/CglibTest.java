7bf4daeeabea79c790501e4ac70e843398d0f866
lijw <lijw@fxqifu.com>
2020-11-05 16:46:57 +0800

    CBLib使用

---
+++ src/main/java/com/jw/myproject/pattern/proxy/cglibproxy/CglibTest.java
@@ -0,0 +1,17 @@
+package com.jw.myproject.pattern.proxy.cglibproxy;
+
+import org.springframework.cglib.core.DebuggingClassWriter;
+
+/**
+ * @author lijw
+ * @date 2020/11/5 11:09
+ */
+public class CglibTest {
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "E://cglib_proxy_classes");
+        Customer obj = (Customer) new CGlibMeipo().getInstance(Customer.class);
+        System.out.println(obj);
+        obj.findLove();
+    }
+}
---
+++ src/main/java/com/jw/myproject/pattern/proxy/cglibproxy/Customer.java
@@ -0,0 +1,12 @@
+package com.jw.myproject.pattern.proxy.cglibproxy;
+
+/**
+ * @author lijw
+ * @date 2020/11/5 11:10
+ */
+public class Customer {
+
+    public void findLove() {
+        System.out.println("儿子要求：肤白貌美大长腿");
+    }
+}
