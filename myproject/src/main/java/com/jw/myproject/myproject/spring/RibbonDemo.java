53215a82a9530ecd6e5f909e984303d7ff29ac02
lijw <lijw@fxqifu.com>
2022-05-18 09:22:47 +0800

    RibbonDemo、招行自动认账时序图

--- myproject/src/main/java/com/jw/myproject/myproject/spring/RibbonDemo.java
+++ myproject/src/main/java/com/jw/myproject/myproject/spring/RibbonDemo.java
@@ -1,18 +1,30 @@
 package com.jw.myproject.myproject.spring;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
+@RestController
 public class RibbonDemo {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
     @LoadBalanced
     @Resource
     private RestTemplate restTemplate;
 
+    @GetMapping("/test")
     public void test() {
-        restTemplate.getForObject("", null);
+        restTemplate.getForObject("http://pangu-member-service/member/employee/48446709", RibbonDemo.class);
     }
 
 }
